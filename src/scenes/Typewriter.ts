6989114159b975b5cfce80e05feb9ba7239e0a5a
liurongqing <phpgege@gmail.com>
2020-08-26 07:57:07 +0800

    字体打字效果

--- src/scenes/Typewriter.ts
+++ src/scenes/Typewriter.ts
@@ -1,16 +1,24 @@
 import { Keys } from '~/consts/index'
 export default class Typewriter extends Phaser.Scene {
   label: Phaser.GameObjects.Text
+  bitmapLabel: Phaser.GameObjects.BitmapText
   constructor() {
     super(Keys.TypewriterScene)
+  }
+
+  preload() {
+    this.load.bitmapFont('atari-smooth-typewrite', 'assets/fonts/atari-smooth.png', 'assets/fonts/atari-smooth.xml')
   }
 
   create() {
 
     this.label = this.add.text(100, 100, '')
       .setWordWrapWidth(100)
-    this.typewriteTextWrapper('Hello, world!')
+    // this.typewriteTextWrapper('Hello, world!')
     // this.typewriteText('Hello, world!')
+
+    this.bitmapLabel = this.add.bitmapText(100, 100, 'atari-smooth-typewrite', '')
+      .setMaxWidth(500)
   }
 
   typewriteText(text: string) {
@@ -25,6 +33,10 @@ export default class Typewriter extends Phaser.Scene {
       repeat: length - 1,
       delay: 200
     })
+  }
+
+  typewriteBitmapText(text: string) {
+
   }
 
   typewriteTextWrapper(text: string) {
